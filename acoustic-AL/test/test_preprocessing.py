4bf3dbfa236389f40db1b049c6345470d7815563
Joel Hoefs <joel.hoefs7@gmail.com>
2024-08-11 05:09:16 +0800

    mess

--- acoustic-AL/test/test_preprocessing.py
+++ acoustic-AL/test/test_preprocessing.py
@@ -14,31 +14,66 @@ import pandas as pd
 from preprocessing import SpectrogramSequence
 import pickle
 
+def dump(o, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(o, f)        
+
+def get(o, filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)      
+
 class TestSequence(unittest.TestCase):
     
     annotations_df: pd.DataFrame = pd.read_csv(ANNOTATIONS / 'initial_dataset_7depl_metadata.csv')
     ds: Dataset = Dataset(DATA_ROOT)
     
     def test_spectrogram_seq(self):
-        
-        self.seq = seq = SpectrogramSequence(self.annotations_df, self.ds)
-        return
-        
-        p = "./objects/seq.pkl"
-        if not Path(p).exists():
-            self.seq = seq = SpectrogramSequence(self.annotations_df, self.ds)
 
-            Path(p).touch()
-            with open(p, 'wb') as f:
-                pickle.dump(seq, f)
-                
-        else:
-            with open(p, 'rb') as f:
-                self.seq = seq = pickle.load(f)
+        # take_pickle = False
+
+        # if take_pickle:    
+        #     p = "./objects/seq.pkl"
+        #     if not Path(p).exists():
+        #         Path(p).touch()
+        #         seq = SpectrogramSequence(self.annotations_df, self.ds, sr=24_000)
+        #         with open(p, 'wb') as f:
+        #             pickle.dump(seq, f)
+        #     else:
+        #         with open(p, 'rb') as f:
+        #             self.seq = seq = pickle.load(f)
+
+        # else:
             
+        # seq = SpectrogramSequence(self.annotations_df, self.ds, sr=24_000)
         
-        # batch = seq.__getitem__(0)
         
+        with open("./objects/seq.pkl", 'rb') as f:
+            self.seq = seq = pickle.load(f)
+
+        batch = seq.__getitem__(0)
+        dump(batch, "./objects/batch.pkl")
+
+        batch = get("./objects/batch.pkl")
+
+        # look at it        
+        import librosa
+        import  matplotlib.pyplot as plt
+        
+        S, y = batch[0]
+        
+        librosa.specshow(S)
+        
+        y_flat = [any(row) for row in y]
+        
+        # NOTE breaks with overlaps
+        diff = np.diff(y_flat)
+        starts = np.where(diff == 1)[0] + 1  
+        ends = np.where(diff == -1)[0] + 1 
+        
+        plt.vlines(starts, label='start')
+        plt.vlines(ends, label='end')
+        plt.savefig("./objects/seq.png")
+        plt.show()
     
 
 if __name__ == '__main__':
